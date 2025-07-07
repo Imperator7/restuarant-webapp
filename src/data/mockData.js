0ba63697e370d5001b22e528927f5ae6dd1cb218
Imperator7 <140708045+Imperator7@users.noreply.github.com>
2025-07-07 18:46:35 +0700

    refactor: separate mockdata

---
+++ src/data/mockData.js
@@ -0,0 +1,7 @@
+export const menuItems = [
+  { id: 1, name: 'Pizza', price: 10.99, picture: 'pizza.jpg' },
+  { id: 2, name: 'Burger', price: 8.99, picture: 'burger.jpg' },
+  { id: 3, name: 'Pasta', price: 12.99, picture: 'pasta.jpg' },
+  { id: 4, name: 'Salad', price: 7.99, picture: 'salad.jpg' },
+  { id: 5, name: 'Soda', price: 1.99, picture: 'soda.jpg' },
+]
