f640477e1c297c021d1920a66f827fd0e05c6e2d
Imperator7 <140708045+Imperator7@users.noreply.github.com>
2025-07-08 17:29:05 +0700

    remove mockData in state

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,7 @@ import Cart from './layout/Cart'
 import Menu from './layout/Menu'
 
 function App() {
-  const [cartOrders, setCartOrders] = useState([
-    { id: 1, name: 'Pizza', amount: 2 },
-  ])
+  const [cartOrders, setCartOrders] = useState([])
 
   return (
     <div style={{ display: 'flex', flexDirection: 'row', gap: '16px' }}>
