3d52c754d1e18b0be6919d2aa12642aac348c185
Imperator7 <140708045+Imperator7@users.noreply.github.com>
2025-07-08 17:59:02 +0700

    ref: creat useCart custom Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,10 @@
 import './App.css'
-import { useState } from 'react'
 import Cart from './layout/Cart'
 import Menu from './layout/Menu'
-import { menuItems } from './data/mockData'
+import useCart from './hooks/useCart'
 
 function App() {
-  const [cartOrders, setCartOrders] = useState([])
-
-  const handleAddToCart = (itemId) => {
-    setCartOrders((prevOrders) => {
-      const existingOrder = prevOrders.find((order) => order.id === itemId)
-      if (existingOrder) {
-        return prevOrders.map((order) =>
-          order.id === itemId ? { ...order, amount: order.amount + 1 } : order
-        )
-      }
-      return [
-        ...prevOrders,
-        {
-          id: itemId,
-          name: menuItems.find((item) => item.id === itemId).name,
-          amount: 1,
-        },
-      ]
-    })
-  }
-
-  const handleRemoveFromCart = (itemId) => {
-    setCartOrders((prevOrders) =>
-      prevOrders
-        .map((order) =>
-          order.id === itemId ? { ...order, amount: order.amount - 1 } : order
-        )
-        .filter((order) => order.amount > 0)
-    )
-  }
-
+  const { cartOrders, handleAddToCart, handleRemoveFromCart } = useCart()
   return (
     <div style={{ display: 'flex', flexDirection: 'row', gap: '16px' }}>
       <Menu handleAddToCart={handleAddToCart} />
