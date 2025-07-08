ed1a4e0ed6b56250efe024532b82fc62e801960b
Imperator7 <140708045+Imperator7@users.noreply.github.com>
2025-07-08 20:14:17 +0700

    add style for cart-button

--- frontend/src/layout/Cart.jsx
+++ frontend/src/layout/Cart.jsx
@@ -2,14 +2,28 @@ import '../styles/Cart.css'
 
 function Cart({ cartOrders, handleAddToCart, handleRemoveFromCart }) {
   return (
-    <div className="Cart">
+    <div className="cart">
       Cart
       <hr style={{ marginBottom: '4px' }} />
       {cartOrders.map((order) => (
-        <div key={order.id}>
-          {order.name} - {order.amount}
-          <button onClick={() => handleAddToCart(order.id)}>+</button>
-          <button onClick={() => handleRemoveFromCart(order.id)}>-</button>
+        <div key={order.id} className="cart-item">
+          <div>
+            {order.name} - {order.amount}
+          </div>
+          <div>
+            <button
+              className="cart-button"
+              onClick={() => handleAddToCart(order.id)}
+            >
+              +
+            </button>
+            <button
+              className="cart-button"
+              onClick={() => handleRemoveFromCart(order.id)}
+            >
+              -
+            </button>
+          </div>
         </div>
       ))}
     </div>
